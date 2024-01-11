4764c15dc264d24bccbdd5ced66dc2b398aa8d1d
DenzelTorrico <denzel_silva@hotmail.com>
2024-01-10 23:12:33 -0500

    nodemon

--- backendnode/index.js
+++ backendnode/index.js
@@ -4,7 +4,7 @@ const app = express();
 const db = require('./db');
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
     next();
   });
 app.get("/books",(req,res)=>{
